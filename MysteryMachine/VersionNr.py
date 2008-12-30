62739117a60c9c65b5a757ccb5d73a6455a6fc18
Roger Gammans <rgammans@computer-surgery.co.uk>
2008-12-30 21:26:54 +0000

    Addition of version number handling class and tests for it

---
+++ MysteryMachine/VersionNr.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#   			VersionNr.py - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+# This file was generated on Tue Dec 30 2008 at 15:39:18
+# The original location of this file is /home/roger/sources/MysteryMachine/generated/VersionNr.py
+#
+#
+
+import types
+from copy import copy
+
+class VersionNr(object):
+
+    """
+     This simple class provide version number arithmetic.
+
+    :author: R G Gammans
+    """
+    def __init__(self,*args):
+	"""
+	Initialiser. Takes a string seperated by '.', a list or
+	another VersionNr.
+	"""
+	if type(args[0]) == type(self):
+		self.SetValue(args[0].nrs)
+	elif type(args[0]) == types.StringType:
+		self.SetValue(args[0].split("."))
+	#Assume list type.
+	else:
+		 self.SetValue(list(args))
+#	assert true "Cannot initialise version Nr"
+    def SetValue(self,list):
+	self.nrs=list
+    def __str__(self):
+	return ".".join(map(str,self.nrs))
+    def __repr__(self):
+	return "VersionNr"+"("+",".join(map(str,self.nrs))+")"
+    def __lt__(self, other):
+        """
+	Basec comparion function to provide an ordering relation.
+
+        @param VersionNr other : value to compare with 
+        @return bool : True if other less than self.
+        """
+	cflist=copy(other.nrs)
+        for level in self.nrs:
+		val=int(level)
+		cfdig=int(cflist.pop(0))
+		#
+		if cfdig == None: return true
+		if cfdig!=val:
+			return val<cfdig
+	#Other must be equal or larger, so
+	return len(cflist)>0
+	
+    def __hash__(self):
+	return hash(self.__repr())
+    def __eq__(self, other):
+        """
+	Equiavalence relation
+
+        @param VersionNr other : value to compare with
+        @return bool : returns if bool version number are equivalent
+        """
+     	cflist=copy(other.nrs)
+        for level in self.nrs:
+		val=int(level)
+		cfdig=int(cflist.pop(0))
+		#
+		if cfdig == None: return False 
+		if cfdig!=val:
+			return False
+	#Other will be the same length if equal
+	# so cf list will be empty now.
+	return (len(cflist) == 0)
+
+pass
+
+
+
---
+++ tests/VernoTest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#   			VersionNr.py - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+# This file was generated on Tue Dec 30 2008 at 15:39:18
+# The original location of this file is /home/roger/sources/MysteryMachine/generated/VersionNr.py
+#
+
+
+"""
+Tests for the MysteryMachine.VersionNr class
+"""
+
+from MysteryMachine.VersionNr import VersionNr
+import unittest
+
+class VernoTest(unittest.TestCase):
+	def testLessThan(self):
+		self.assert_( VersionNr(3,1) < VersionNr(4,1) )
+		self.assert_( VersionNr(3)   < VersionNr(3,1))
+		self.assert_( VersionNr(3,1) < VersionNr(4,1))
+		self.assert_( VersionNr(3)   < VersionNr(3,1))
+
+	
+	def testGreaterThan(self):
+		self.assert_( VersionNr(4,1) > VersionNr(3,1) )
+		self.assert_( VersionNr(3,1) > VersionNr(3) )
+
+	def testEquals(self):
+		self.assert_( VersionNr(3) == VersionNr(3) )
+		self.assert_( VersionNr(3,1) == VersionNr("3.1") )
+
+	def testCopy(self):
+		a=VersionNr(3,1)
+		b=VersionNr(a)
+		self.assertEqual( a , b )
+
+
+def getTestNames():
+	return [ 'VernoTest.VernoTest' ] 
+
+if __name__ == '__main__':
+    unittest.main()
+
