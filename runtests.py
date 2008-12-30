2618d8052d5a8f4f4372d8981996f1ef09a3c7b2
Roger Gammans <rgammans@computer-surgery.co.uk>
2008-12-30 14:05:09 +0000

    Add basic testing script

---
+++ runtests.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# First cut of a routine to run our unittests for the
+# MysteryMachine Project
+
+
+#   			runtests.sh - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+import os
+import imp
+import sys
+import re
+
+PYTHONS  =( 'python2.5', 'python2.6', 'python2.6 -3')
+UNITTEST ='/usr/lib/python2.6/unittest.py'
+TESTSDIR ='tests'
+
+STRIP   = re.compile('\.py$')
+INVALID = re.compile('\.py[co~]$')
+
+sys.path.insert(0,"./%s" % TESTSDIR )
+
+def NoTests():
+	return( )
+
+for python in (PYTHONS):
+	print "Testing under %s:\n" % python
+	#TODO: should this inner loop go inside tests/__init__.py ?
+	for module in os.listdir('tests/'):
+		#Turn filenmae into module name
+		if re.search(INVALID,module) != None: continue
+		testname=re.sub(STRIP,"",module)
+		#Load module.
+		fp,pathname,desc=imp.find_module(testname)
+		ThisMod=imp.load_module(testname,fp,pathname,desc)
+		#safely Query module about available tests
+		func=getattr(ThisMod,'getTestNames',NoTests)
+		testsAvail=func()
+		if len(testsAvail) > 0 :
+			#If we have some available tests then run them.
+			testList=" ".join(testsAvail);
+			print testList
+			#FIXME: Adjust python path for the new python.
+			os.putenv("PYTHONPATH","./%s"%TESTSDIR)
+			os.system("%s %s %s" % ( python, UNITTEST, testList))
+
