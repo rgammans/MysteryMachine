6478b15e3f861ab0528e0a5513ceb6bb4b58f2bc
Roger Gammans <rgammans@computer-surgery.co.uk>
2009-02-07 14:18:21 +0000

    * hadd ehading and secure ID class and tests

---
+++ MysteryMachine/ExtensionSecureID.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#   			ExtensionSecureID.py - Copyright Roger Gammans 
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+#
+#
+
+import hashlib
+import os
+
+class ExtensionSecureID(object):
+
+  """
+   This class represents a secure hash for an extension. Trust of one version of an
+   extension is not transitive to earlier or later versions.
+
+   This base class uses SHA256 as the hash function
+
+  :version: 1.0
+  :author:  R G Gammans
+  """
+
+
+  @classmethod
+  def fromPathName(cls,path):
+    input= file(path)
+    hash = hashlib.sha256() 
+    for chunk in input:
+      hash.update(chunk)
+    return cls(path,hash,"sha256")
+
+  @classmethod
+  def fromHash(cls,path,hashstring):
+     return cls(path,hashstring) 
+    
+  def __init__(self, path, hashstr, hashtype ="sha256"):
+    """
+     Computes the signature of an extension from it's path.
+
+    @param string path : Pathname of extension to compute signature of.
+    @author
+    """
+    self._path= path
+
+    if isinstance(hashstr,basestring):
+      self._hashtype , self._value = hashstr.split("|")
+      self._hash = hashlib.new(self._hashtype)
+    else:
+      self._hash= hashstr
+      self._value=hashstr.hexdigest()
+      self._hashtype=hashtype
+
+
+  def __repr__(self):
+    return "%s(%s,%s|%s)" % (self.__class__ , self._path , self._hashtype , self._value ) 
+
+  def __str__(self):
+    return "%s|%s" % (self._hashtype, self._value )
+
+  def __eq__(self, other):
+    """
+  
+    @param ExtensionSecureID other : 
+    @return bool :
+    """
+    return self._value == other._value
+
---
+++ tests/ExtSecIdTest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#   			VersionNr.py - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+# This file was generated on Tue Dec 30 2008 at 15:39:18
+# The original location of this file is /home/roger/sources/MysteryMachine/generated/VersionNr.py
+#
+
+
+"""
+Tests for the MysteryMachine.ExtensionSecureID class
+"""
+
+from MysteryMachine.ExtensionSecureID import ExtensionSecureID
+import unittest
+
+class ExtSecIdTest(unittest.TestCase):
+	def setUp(self):
+		self.cfvalue= str("sha256|230550d1627270f0cf2ff8bec59679a6079266d3c68ac3e1653cd0a97276fdec" )
+		self.testpath="CodingGuidelines"
+	def testBasic(self):
+		hash1=ExtensionSecureID.fromPathName(self.testpath)
+		hash2=ExtensionSecureID.fromPathName(self.testpath)
+		self.assertEquals( hash1 , hash2 )
+
+	def testStr(self):
+		a=ExtensionSecureID.fromPathName(self.testpath)
+		self.assertEqual( str(a) ,self.cfvalue)
+
+	def testKnownHash(self):
+		hash1=ExtensionSecureID.fromHash(self.testpath, self.cfvalue)
+		self.assertEqual( str(hash1) , self.cfvalue )
+		hash2=ExtensionSecureID.fromPathName(self.testpath)
+		self.assertEqual(hash1 , hash2 )
+
+def getTestNames():
+	return [ 'ExtSecIdTest.ExtSecIdTest' ] 
+
+if __name__ == '__main__':
+    unittest.main()
+
