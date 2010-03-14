abd6ec547c22fda745514e1fef99d7e7b77a97be
Roger Gammans <rgammans@computer-surgery.co.uk>
2010-03-14 17:21:02 +0000

    Add an implementation of a list Attribute type.   - This also provides our only ordered relation in the     entire schema.

---
+++ MysteryMachine/schema/MMListAttribute.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+#   			MMListAttribute.py - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+
+from MysteryMachine.schema.MMAttributeValue import MMAttributeValue ,MakeAttributeValue , CreateAttributeValue 
+
+
+#If we deprecated py2.5 this mess can be replaced
+#with from 'itertools import izip_longest'
+import itertools
+import functools
+
+# izip_longest only exists from2.6 onwards 
+# but  map(None,.) does the same but is deprecated
+# in 3.x
+try:
+    izip_longest = itertools.izip_longest
+except:
+    izip_longest = functools.partial(map,None)
+
+
+
+
+class MMListAttribute(MMAttributeValue):
+    """
+    An attribute value type which provides an ordered list of strings.
+    """
+    typename = "list"
+    contain_prefs = { list: 100 }
+
+    def __init__(self,*args,**kwargs):
+        super(MMListAttribute,self).__init__(self,*args,**kwargs)
+        if self.value is not None:
+            #Value overrides parts..
+            self.parts = { }
+            for item in self.value:
+                self.append(item)
+
+        self.exports+= [ "__contains__","__getitem__","__setitem__","__delitem__","count","extend","insert","append" ]
+
+    def __contains__(self,x , obj = None ):
+        return x in self.parts.values()
+
+
+    def _get_key(self,index,obj = None):
+        return sorted(self.parts.keys())[int(index)]
+
+    def __delitem__(self,index ,obj = None):
+        key = self._get_key(index,obj)
+        del self.parts[key]
+
+    def __getitem__(self,index , obj = None):
+        key = self._get_key(index,obj)
+        return self._convert_to_val(key,self.parts[key])        
+
+    def __setitem__(self,index,value , obj = None):
+        key = self._get_key(index,obj)
+        self._write(key,value,obj)
+
+    def count(self,obj = None):
+        return len(self.parts)
+
+    def extend(self, iter , obj = None):
+        for i in iter:
+            self.append(i)
+
+    def insert(self,index,item , obj = None):
+        klist = sorted(self.parts.keys())
+        if index >0:
+            key_a = klist[index-1]
+        else:
+            key_a = None 
+        key_b = klist[index]
+        key   = str(_Key(key_a).between(key_b))
+        self._write(key,item,obj)
+
+    def append(self,item, obj = None):
+        key = self._key_append()
+        self._write(key,item,obj)
+
+    def _key_append(self):
+        lastkey = None
+        try:
+            lastkey = sorted(self.parts.keys(),reverse=True)[0]
+        except:
+            pass
+ 
+        lastkey = _Key(lastkey)
+        lastkey = lastkey.next()
+
+        return str(lastkey)
+
+    def _write(self,key,value,obj = None):
+        self.parts[key] = self._convert_to_str(key,value,obj)
+        if obj is not None:
+            obj._writeback()
+
+    def _convert_to_str(self,key , v, obj = None):
+        #Force value Into an MMAttributeValue Object
+        v = CreateAttributeValue(v,copy = False)
+        if len(v.get_parts()) != 1:
+            raise TypeError("MMListAttribute only supports single part values")
+        
+        value = v.get_parts().iteritems().next()
+        #We can safely use ':' as a seperator as it isn't
+        # allowed in  type or part names.
+        return v.get_type()+":"+value[0]+":"+value[1]
+
+    def _convert_to_val(self, key ,v, obj = None):
+        #Limit split so that we don't lose data after ':' in the 
+        #value.
+        data = v.split(":",2)
+        return MakeAttributeValue(data[0],
+                                  parts= { data[1]: data[2]} )
+
+
+class _Key(object):
+    """
+    Encodes the actually logic used for generating dict keys for
+    our ordered list.
+    """
+    """
+    This class uses a tree analogy to create sortable strings for
+    our dict elements.
+    On each subtree self.order[0] and self.order[-1] are reserved to
+    be immediately extended creating a new subtree
+    Under normal circumstances we try to bisect the difference between the
+    end (self.order[-1] in the case of append) and the start items.
+
+    However if the two elements are 'adajenct' we create another subtree
+    (append another character) and start from the center there.
+
+    eg. (using a,b,c,d,e) as our symbols. 'a' and 'e' are reserved as
+        our endpoints. and we start with 'c'
+        
+        append           -> c
+                            c
+        append           -> c , d
+                            c--d
+        insert(c)        -> c , cc ,d
+                       c--d
+                       |
+                       cc
+        insert(c)        -> c, cb , cc, d
+                       c--+--d
+                       |
+                    cb-+-cc
+        insert(c)        -> c , cac,cb,cc,d
+                       c--+--d
+                       |
+                 (ca)-cb-+-cc
+                  |
+                 cac
+
+    Note: We can't use the ca node as it is impossible to insert anything
+          between the 'c' node and the 'ca' node.
+    """
+    #This _must_ be longer than three elements, otherwise the algorythm
+    # cant always guarantee to find a between. Ideally it should be 3**n .
+    #But I've compromised on 26 - one short of the ideal for n=3, becuase a-z
+    # have a stable sort order across most (hopefully all) locales.
+    order = [  ] 
+    for i in range(26): 
+        order.append(chr(ord('a') + i))
+
+    #VV ---  Simple to trick to ensure algo requiremnt is true --- VV
+    order = sorted(order)
+    ##################################################################
+
+    def __init__(self,value = None):
+        self.abet =  self.__class__.order
+        self.value = value
+        if self.value is None:
+            self.value = ''
+ 
+    def __str__(self):
+        return self.value
+
+    def between(self,other):
+        """
+        Return a key half way between this and other - assuming no other 
+        intermediate keys exist in the tree.
+        """
+        ans = []
+        for lo,hi in izip_longest(self.value,other):
+
+            if lo is None: lo = self.abet[0]
+            if hi is None: hi = self.abet[-1]
+
+            if lo == hi:
+                #The nodes are on the same subtree - so the answer will be too.
+                ans.append(lo)
+            else:
+                #At this point we have found a difference between the subtrees which
+                #contain the nodes.
+                #But consider:-
+                #           -b---------d-
+                #            |         |
+                #        bb--bc--bd    dc-dd
+                #
+                # I think it is sensible to return 'c' for insert_after('bd')
+                new = self.abet[ ( self.abet.index(hi) + 
+                                   self.abet.index(lo) ) //2 ]
+               
+                #Check is unique and not reserved.
+                if hi != new and lo != new and new !=0 and new != len(self.abet)-1:
+                    ans.append(new)
+                else:
+                    #New subtree - ensure goes in a good place new == hi is the
+                    #              only possible problem so lets trap it.
+                    #      (It shouldn't occur due to integer division but..)
+                    if new == hi: new=lo
+                    ans += [ new , self.abet[len(self.abet) // 2] ]
+                
+                break
+
+        return self.__class__("".join(ans))
+
+    def next(self):
+        """
+        The implicit assumption is that this is the last key in the squenence.
+        
+        We need to return another key somewhere later in the sequence. We could
+        use between() here but that wouldn't make the most use of the end element. 
+   
+        So we use a slightly customized algorithm to do between(self,self.abet[-1])
+        """
+
+        if self.value == '':
+            #Special case to handle empty string
+            return self.__class__(self.abet[len(self.abet) // 2])
+
+        ans = [ ]
+        got = False
+        for digit in self.value:
+            if digit != self.abet[-1]:
+                # Find digit 1/2 to the end rounding up (allowing us to use the end node)
+                new = self.abet[ ( self.abet.index(digit) +
+                                   len(self.abet)-1 )  // 2 ]
+                
+                #Run out of space on this subtree/node
+                if new == digit:
+                    # Last node on tree: - 
+                    #   use Create mid point in new subtree
+                    ans += [self.abet[-1] , self.abet[len(self.abet) // 2] ]
+                else:
+                    ans += [ new ]
+                #No need to add any more subtrees - so exit here.
+                got = True
+                break
+            else:
+                #On an end subtree move down and consider it alone...
+                ans += [ digit ]
+        
+        #We must have finished succesfully as got can only be false if
+        # if we have an unbroken sequenence of self.abet[-1]'s but we 
+        # never generate such a sequence. 
+        assert(got)
+        return self.__class__("".join(ans))
+
---
+++ tests/listValueTest.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+#   			grammarTest.py - Copyright Roger Gammans
+# 
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# 
+"""
+Tests for the MysteryMachine.Schema AttributeValue module
+"""
+
+from MysteryMachine.schema.MMListAttribute import * 
+from MysteryMachine.schema.MMListAttribute import _Key as ListKey 
+from MysteryMachine.schema.MMAttributeValue import * 
+from MysteryMachine.schema.MMAttribute import * 
+import unittest
+from MysteryMachine import * 
+from itertools import izip
+
+class SystemProxy: 
+    def get_object(self,cat,id):
+        return ObjectProxy(cat , id)
+    def getSelf(self):
+        return self
+
+class DummyParser:
+    def GetString(self,string,node):
+        return string
+
+class ObjectProxy(dict):
+    def get_parser(self):
+       return DummyParser() 
+
+
+    
+class ListValTest(unittest.TestCase):
+    def setUp(self):
+       StartApp(["--cfgengine=ConfigDict", "--cfgfile=test.cfg", "--testmode"]) 
+    
+    def testList(self):
+        val  = MMListAttribute(value =  [ "first" , "second" , "third" ] )
+        self.assertEquals(str(val[0] )  , "first")      
+        self.assertEquals(str(val[1] )  , "second")      
+        self.assertEquals(str(val[2] )  , "third")      
+        val.append("fourth")
+        self.assertEquals(val.count(),4)
+        self.assertEquals(str(val[3])  , "fourth")
+        val.insert(2,"2 and half'th")
+        self.assertEquals(str(val[0] )  , "first")      
+        self.assertEquals(str(val[1] )  , "second")      
+        self.assertEquals(str(val[2])  ,  "2 and half'th")
+        self.assertEquals(str(val[3])   , "third")      
+        self.assertEquals(str(val[4])   , "fourth")      
+        self.assertEquals(val.count(),5)
+        val.insert(0,"zeroth")
+        self.assertEquals(str(val[0] )  , "zeroth")      
+        self.assertEquals(str(val[1] )  , "first")      
+        self.assertEquals(str(val[2] )  , "second")      
+        self.assertEquals(str(val[3] ) ,  "2 and half'th")
+        self.assertEquals(str(val[4] )  , "third")      
+        self.assertEquals(str(val[5] )  , "fourth")  
+        self.assertEquals(val.count(),6)
+        del val[4]
+        self.assertEquals(str(val[0] )  , "zeroth")      
+        self.assertEquals(str(val[1] )  , "first")      
+        self.assertEquals(str(val[2] )  , "second")      
+        self.assertEquals(str(val[3])  ,  "2 and half'th")
+        self.assertEquals(str(val[4])   , "fourth")  
+        self.assertEquals(val.count(),5)
+        val[3]="third" 
+        self.assertEquals(str(val[0] )  , "zeroth")      
+        self.assertEquals(str(val[1] )  , "first")      
+        self.assertEquals(str(val[2] )  , "second")      
+        self.assertEquals(str(val[3] )  ,  "third")
+        self.assertEquals(str(val[4])   , "fourth")  
+        self.assertEquals(val.count(),5)
+
+    def testAttributeForward(self):
+        val  = MMListAttribute(value =  [ "first" , "second" , "third" ] )
+        obj  = ObjectProxy()
+        attr = MMAttribute("test",val,obj)
+        self.assertEquals(attr.count() ,3 )        
+        self.assertEquals(str(attr.__getitem__(1)) , "second" )        
+        self.assertEquals(str(attr[1]) ,"second" )        
+        self.assertEquals(str(attr["1"]) ,"second" )        
+        self.assertEquals(str(attr["-1"]) ,"third" )        
+        del attr["1"]
+        self.assertEquals(attr.count() ,2 )        
+        self.assertEquals(str(attr["1"]) ,"third" )        
+        #Check writeback occurred.
+        self.assertEquals(obj["test"],val)
+        valo = CreateAttributeValue("primary")
+        attr["0"] = valo
+        self.assertEquals(attr[0].get_value(),valo) 
+ 
+        #Clear dict so can check writeback again
+        del obj["test"]
+        attr[1]="second"
+        self.assertEquals(str(attr["1"]) ,"second" )        
+        self.assertEquals(obj["test"],val)
+        #Check walk back thru and find a parser
+        self.assertEquals(attr[1].GetFullExpansion(),"second")
+
+        del obj["test"]
+        attr.append("some fate")
+        self.assertEquals(obj["test"],val)
+        self.assertEquals(str(attr["2"]) ,"some fate" )        
+
+        del obj["test"]
+        attr.insert(2,"some date")
+        self.assertEquals(obj["test"],val)
+        self.assertEquals(str(attr["2"]) ,"some date" )        
+        self.assertEquals(str(attr["3"]) ,"some fate" )        
+
+
+        #Check trying insert an invalid class.
+        class dummy (MMAttributeValue):
+            typename = "dummy"
+            contain_prefs = {}
+         
+        def test(a):
+            a[2] = dummy(parts = {'f':"testing", 'g':"123"})
+                   
+        self.assertRaises(TypeError,test,attr)
+
+
+    def testKey(self):
+        a = ListKey()
+        self.assertFalse(a is None)
+        #FIXME Add asserion about the empty/null position.
+
+        ## Now test next generates increasing values..
+        a = a.next()
+        b = a
+        
+        #Val list to use to test insertion too!.
+        vallist = [ ListKey() , a ]
+        
+        # Check  string as length increases too..
+        for i in xrange(3):
+            a = b
+            while len(str(a)) >= len(str(b)):
+                c = b
+                b = b.next()
+                vallist.append(b)
+                self.assertTrue(str(b) > str(c))
+        
+        ## Check between creates useful values.
+        for ele1,ele2 in izip(vallist,vallist[1:]):
+            b = ele2
+            a = ele1
+            for i in xrange(4):
+                a = b
+                while len(str(a)) >= len(str(b)):
+                    c = b
+                    b = ele1.between(str(c))
+                    self.assertTrue(str(b) > str(ele1))
+                    self.assertTrue(str(c) > str(b))
+ 
+def getTestNames():
+	return [ 'listValueTest.ListValTest' ] 
+
+if __name__ == '__main__':
+    unittest.main()
+
