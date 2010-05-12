f793f6ecea715b80cff88e84638a1e5051721a1a
Roger Gammans <rgammans@computer-surgery.co.uk>
2010-05-12 20:25:35 +0100

    Substatially improvements to the installer to deal with more basic python environments.
    Additionally some actually tested instructions in teh README file

--- pavement.py
+++ pavement.py
@@ -19,19 +19,57 @@
 #
 
 
+#
+# Find the current state of setuptools..
+#
 
-DIST_PACKAGES = ["yapsy" , "MysteryMachine","MysteryMachine.schema",
+
+existing_setuptools_available   = False
+try:
+    import setuptools
+    existing_setuptools_available = True
+except:
+    pass
+
+ez_setup_available = False
+try:
+   import ez_setup
+   version = ez_setup.DEFAULT_VERSION
+   if existing_setuptools_available:
+        version = setuptools.__version__
+   ez_setup.use_setuptools(version = version)
+   ez_setup_available = True
+except ImportError:
+   pass
+
+easy_install_available = False 
+try:
+    import setuptools.command.easy_install
+    easy_install_available = True
+except ImportError:
+    pass
+
+
+DIST_PACKAGES = [ "yapsy" , "MysteryMachine","MysteryMachine.schema",
                  "MysteryMachine.store","MysteryMachine.parsetools",
                  "MysteryMachine.Ui","MysteryMachine.utils",
                  "MysteryMachine.policies" , "MysteryMachine.document"]
 
-EZ_PACKAGES   = ["docutils" , "mercurial" , "bpython" , "pyparsing > 1.5" ]
+PY_MODULES  = [ 'pyparsing' ]
+
+#Mercurial and pyaprsing (as of 1.5.2 ) don't install with easy_install. 
+EZ_PACKAGES   = ["docutils" , 
+#                 "mercurial" ,
+                 "bpython" , 
+#                 "pyparsing > 1.5" ,
+                 "PyYaml"]
+
 
 TESTSDIR = "tests"
 FILES = [ "AUTHORS", "CodingGuidelines", "OriginalDesign" ,"README",
           "paver-minilib.zip" ]
 DIRS  = ["docs" , "examples" ,"graphics", "scripts" ,"patches" , TESTSDIR]
-SCRIPTS = [ 'scripts/mysterymachine', 'setup.py' ]
+SCRIPTS = [ 'scripts/mysterymachine', 'setup.py' , 'ez_setup.py' ]
 
 VENVSCRIPT = 'install.py'
 
@@ -39,10 +77,11 @@ PYTHONS= [ "python" ]
 
 import sys
 import os
+import shutil
+import atexit
 
 from paver.easy import *
 from paver.setuputils import setup
-import paver.virtual
 
 def find_all(path,dirname):
     for dir,dirs,files in os.walk(path):
@@ -56,10 +95,11 @@ def find_all(path,dirname):
 
 
 #
-# Redefine minilib so that it include the virtual module
+# Redefine minilib so that it includes the virtual module
 # 
 @task
 def minilib(options):
+    #paver seems to complain if this is already there..
     if 'extra_files' not in options:
         options['extra_files']= [ ]
     options['extra_files'] += ['virtual']
@@ -67,7 +107,7 @@ def minilib(options):
 
 
 @task
-@needs('generate_setup','minilib')
+@needs('generate_setup','minilib','src_environ')
 def sdist():
     """Create a source distribution (tarball, zip file, etc.)"""
     global SCRIPTS
@@ -76,12 +116,26 @@ def sdist():
     path = sys.path
     path.insert(0,".")
     for pkg in DIST_PACKAGES:
-        pkgdir = pkg.split(".")
-        for dir in sys.path:
+       pkgdir  = pkg.split(".")
+       for dir in sys.path:
             if os.path.isdir(os.path.join(dir,*pkgdir)):
                try:
                    #Put all our need packages in the local directory.
                    os.symlink(os.path.join(dir,*pkgdir),os.path.join(pkgdir[0],*pkgdir[1:]))
+               except:
+                   pass
+
+    for pkg in PY_MODULES:
+        filedir = pkg.split(".")
+        filedir[-1] = filedir[-1]+ ".py"
+        for dir in sys.path:
+            #Check for package as standalone file
+            if os.path.exists(os.path.join(dir,*filedir)):
+               try:
+                   #Put all our need packages in the local directory 
+                   shutil.copy(os.path.join(dir,*filedir),os.path.join(filedir[0],*filedir[1:]))
+                   #Register deletion of new filefor when we've finished..
+                   atexit.register(os.remove,os.path.join(filedir[0],*filedir[1:]))
                except:
                    pass
 
@@ -93,6 +147,12 @@ def sdist():
     SCRIPTS += FILES
     SCRIPTS += VENVSCRIPT 
 
+    #If ez_setup.py is not on our sys.path.
+    if not ez_setup_available:
+        import urllib
+        urllib.urlretrieve("http://peak.telecommunity.com/dist/ez_setup.py","ez_setup.py")
+         
+
     paver.virtual.bootstrap() 
     call_task("setuptools.command.sdist")
 
@@ -103,10 +163,9 @@ def test():
     """Run MysteryMachine unit test on the default python"""
     path=os.getenv("PYTHONPATH")
     if path == None:
-    	path = "."
+    	path = os.curdir 
     else:
-    	#FIXME: Needs to be ';' under windows
-    	path = path+":."
+    	path = path+os.pathsep+os.curdir
     
     os.putenv("PYTHONPATH",path)
 
@@ -123,10 +182,70 @@ def test():
 		os.system("%s %s/%s" % ( python,  TESTSDIR, module))
 
 
+
+def relaunch_self(extra_args):
+    """Relaunches this process with additional arguments"""
+    args = [ sys.executable ] + sys.argv + extra_args
+    #Start again, 
+    os.execv(sys.executable,args)
+
 @task
-@needs('setuptools.command.install')
+#should install mercurial
+def dst_environ(options):
+    """Install MysteryMachine dependencies that setuptools can't handle"""
+    #Check  whether we need pyparsing...
+    try:
+        import pyparsing
+        v = pyparsing.__version__.split(".")
+        for existing,minimum in zip(v,['1','5','0']):
+            if existing < minimum: raise ImportError()
+        #pyparsing exists in a usable version - don't override.
+        options.setup.py_modules.remove('pyparsing') 
+    except ImportError:
+        #We need the shipping version of pyparsing with mysterymachine
+        pass
+
+    #TODO Install mercurial
+
+@task 
+@cmdopts([('srcenv-loop=', 'l' ,'internal use only - used to detect infinite loops')])
+def src_environ(options):
+    """Install packages necessary for the installer in the source environment"""
+    print "verifying src environment"
+    try:
+        import virtualenv
+        print "virtual env already here - nothing to do"
+    except ImportError:
+        if int(options.src_environ.get('srcenv_loop',0)) == 1:
+            raise LogicError("Virualenv install loop detected")
+        print "trying to install virtualenv"
+        if easy_install_available:
+            print ".with easy_install"
+            print "calling easy_install"
+            old_args = sys.argv
+            sys.argv = ['fake', 'virtualenv']
+            import setuptools.command.easy_install
+            setuptools.command.easy_install.main()
+            sys.argv = old_args
+        else:
+            print "...giving up - can't install virtualenv"
+            
+    if sys.platform[:3] == 'win':
+        #Bpython doesn't work under windows to remove it from the
+        # deps list
+        options.virtualenv.packages_to_install.remove('bpython')
+        options.setup.install_requires.remove('bpython')
+      
+    #Hope all is well
+    print "importing paver virtual"
+    import paver.virtual
+    if not paver.virtual.has_virtualenv:
+        relaunch_self(["--srcenv-loop","1"])
+
+@task
+@needs('dst_environ')
 def install_here():
-    """Install everything from the build dir into the current env.
+    """Install everything from the build dir into t)he current env.
    
     This does the usual install thing installing all of the MysteryMachine
     packages and there dependencies into the current environment.
@@ -134,12 +253,11 @@ def install_here():
     If you wish to install MysteryMachine into your global environement
     this /might/ be want you want.    
     """
-    #Dummy script for rename effect setuptools should do everything required.
-    pass
+    call_task('setuptools.command.install')
 
 
 @task
-@needs('paver.virtual.bootstrap')
+@needs('src_environ')
 @cmdopts([('installdir=', 'd' ,'Directory to install MysteryMachine into')])
 def install(options):
     """Create a virtual environment and install into it.
@@ -153,6 +271,11 @@ def install(options):
     """
     #options(virtualenv = Bunch(dest_dir = options.install.installdir,
     #                           paver_command_line = "install_here"))
+
+    # We can't add this a a dependencies - because it needs virtualenv
+    # which might not exist and dependency resolution.
+    call_task('paver.virtual.bootstrap')
+
     here = os.getcwd()
     try:
         os.mkdir(options.install.installdir)
@@ -161,17 +284,24 @@ def install(options):
 
     os.chdir(options.install.installdir)
     #call Pre built install file , creates virtual environment etc..
-    os.system(sys.executable + " " + os.path.join(here,"install.py"))
+    import subprocess
+    subprocess.call(sys.executable , os.path.join(here,"install.py") )
     os.chdir(here)
     #Relaunch paver to install ourself into the newly setup virtualenv.
-    os.system(os.path.join(options.install.installdir,"bin","paver")+ " -f "+
-              os.path.join(here,"pavement.py")+" install_here")
+    # -but first find our paver executable..
+    paver =os.path.join(options.install.installdir,"bin","paver")
+    try:
+	    os.stat(paver)
+    except WindowsError,OSError:
+	    paver = os.path.join(options.install.installdir,"scripts","paver")
+    subprocess.call(paver,"-f",os.path.join(here,"pavement.py"),"install_here")
 
 
 setup(name ="MysteryMachine",
-      packages = DIST_PACKAGES,
-      scripts  = SCRIPTS,
-      version  = "0.1.0",
+      packages = DIST_PACKAGES ,
+      scripts  = SCRIPTS ,
+      py_modules =PY_MODULES,
+      version  = "0.1.1",
       install_requires = EZ_PACKAGES,
       url="http://trac.backslashat.org/MysteryMachine",
       author="Roger Gammans",
@@ -185,8 +315,5 @@ options(
     virtualenv = Bunch(
         script_name = VENVSCRIPT, 
         packages_to_install = EZ_PACKAGES,
-#       Removed as there is no practical way to 
-#       predetermie a path back to this pavement file
-#        paver_command_line  = " install_here",
     )
 )
